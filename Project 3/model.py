926294dff5144aa2d42b9600699c66aef8d54870
MighTy-Weaver <1874240442@qq.com>
2022-05-23 19:24:19 +0800

    project 3 update

--- Project 3/model.py
+++ Project 3/model.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 
@@ -23,6 +24,30 @@ class NN_regressor(nn.Module):
 
 
 class NN_regressor_v2(nn.Module):
-    def __init__(self, in_dim_1, in_dim_2, embedding_dim=798, out_dim=1):
+    def __init__(self, in_dim_1=6912, in_dim_2=22, out_dim=1):
         super(NN_regressor_v2, self).__init__()
+        self.flatten = nn.Flatten()
+        self.nn1 = nn.Sequential(nn.Linear(in_features=in_dim_1, out_features=2048), nn.BatchNorm1d(num_features=2048),
+                                 nn.Dropout(0.2))
+        self.nn2 = nn.Sequential(nn.Linear(in_features=2048, out_features=512), nn.BatchNorm1d(num_features=512),
+                                 nn.Dropout(0.2))
+        self.nn3 = nn.Sequential(nn.Linear(in_features=512, out_features=128), nn.BatchNorm1d(num_features=128),
+                                 nn.Dropout(0.2))
+        self.nn4 = nn.Sequential(nn.Linear(in_features=128, out_features=32), nn.BatchNorm1d(num_features=32),
+                                 nn.Dropout(0.2))
+        self.feature_nn1 = nn.Sequential(nn.Linear(in_features=in_dim_2, out_features=32),
+                                         nn.BatchNorm1d(num_features=32), nn.Dropout(0.2))
+        self.final_nn1 = nn.Sequential(nn.Linear(in_features=64, out_features=16), nn.BatchNorm1d(num_features=16),
+                                       nn.Dropout(0.2))
+        self.final_nn2 = nn.Linear(in_features=16, out_features=out_dim)
 
+    def forward(self, embedding_x, feature_x):
+        out1 = self.flatten(embedding_x)
+        out1 = self.nn1(out1)
+        out1 = self.nn2(out1)
+        out1 = self.nn3(out1)
+        out1 = self.nn4(out1)
+        out2 = self.feature_nn1(feature_x)
+        out = torch.cat([out1, out2], dim=-1)
+        out3 = self.final_nn1(out)
+        return self.final_nn2(out3)
---
+++ Project 3/train2.py
@@ -0,0 +1,130 @@
+import argparse
+import os
+from math import sqrt
+
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.metrics import mean_squared_error
+from torch import nn
+from torch.optim import AdamW
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from tqdm import trange
+from transformers import BertModel
+from transformers import BertTokenizer
+from transformers import get_scheduler
+
+from dataloader import YelpDataset_v2
+from model import NN_regressor_v2
+
+
+def rmse(pred, actual):
+    # Ignore nonzero terms.
+    pred = pred[actual.nonzero()].flatten()
+    actual = actual[actual.nonzero()].flatten()
+    return sqrt(mean_squared_error(pred, actual))
+
+
+# Set up a argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--gpu", type=int, default=1, required=False, help="The number of gpu you want to use")
+parser.add_argument("--epoch", type=int, default=300, required=False)
+parser.add_argument("--bs", type=int, default=512, required=False)
+parser.add_argument("--lr", type=float, default=5e-5, required=False)
+parser.add_argument("--test", type=int, default=0)
+args = parser.parse_args()
+
+# Set the GPU device
+if torch.cuda.is_available():
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
+    torch.cuda.set_device(args.gpu)
+
+# Check device status
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('CUDA available:', torch.cuda.is_available())
+print(torch.cuda.get_device_name())
+print('Device number:', torch.cuda.device_count())
+print(torch.cuda.get_device_properties(device))
+
+# Set up some parameter we can use
+epochs = args.epoch
+BS = args.bs
+LR = args.lr
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+encoder = BertModel.from_pretrained('bert-base-uncased').to(device)
+
+train_dataset = YelpDataset_v2('train', encoder=encoder, tokenizer=tokenizer, device=device, test=args.test)
+valid_dataset = YelpDataset_v2('valid', encoder=encoder, tokenizer=tokenizer, device=device, test=args.test)
+test_dataset = YelpDataset_v2('test', encoder=encoder, tokenizer=tokenizer, device=device, test=args.test)
+
+model = NN_regressor_v2().to(device)
+
+train_loader = DataLoader(train_dataset, batch_size=BS, shuffle=True)
+valid_loader = DataLoader(valid_dataset, batch_size=BS, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=len(test_dataset), shuffle=False)
+
+criterion = nn.MSELoss()
+optimizer = AdamW(list(model.parameters()) + list(encoder.parameters()), lr=LR)
+
+lr_scheduler = get_scheduler(name='linear', optimizer=optimizer,
+                             num_warmup_steps=0, num_training_steps=epochs * len(train_loader))
+
+min_RMSE = 999999
+
+for e in trange(epochs, desc="Epoch: "):
+    epoch_loss = 0
+
+    model.train()
+    train_label, train_pred = [], []
+    for input1, input2, labels in tqdm(train_loader, desc="Train batch: "):
+        input1 = input1.to(device)
+        input2 = input2.to(device)
+        labels = labels.to(device)
+        outputs = model(input1, input2).squeeze()
+        # print(outputs.shape,labels.shape)
+        # print(outputs)
+        # print(labels)
+        loss = torch.sqrt(criterion(outputs, labels))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        train_label.extend(labels.detach().cpu().tolist())
+        train_pred.extend(outputs.detach().cpu().tolist())
+    train_rmse = rmse(np.array(train_pred), np.array(train_label))
+    model.eval()
+
+    valid_pred, valid_label = [], []
+    for input1, input2, labels in tqdm(valid_loader, desc="Valid batch"):
+        input1 = input1.to(device)
+        input2 = input2.to(device)
+        labels = labels.to(device)
+        outputs = model(input1, input2).squeeze()
+        valid_loss = torch.sqrt(criterion(outputs, labels))
+
+        valid_pred.extend(outputs.detach().cpu().tolist())
+        valid_label.extend(labels.detach().cpu().tolist())
+    valid_rmse = rmse(np.array(valid_pred), np.array(valid_label))
+    if valid_rmse <= min_RMSE:
+        min_RMSE = valid_rmse
+        if not args.test:
+            valid_csv = pd.read_csv('./data/valid.csv', index_col=None)
+            valid_csv['stars_pred'] = valid_pred
+            valid_csv.to_csv('./data/valid_pred.csv', index=False)
+
+        y_test_labels = []
+        for input1, input2 in tqdm(test_loader, desc="Test batch: "):
+            input1 = input1.to(device)
+            input2 = input2.to(device)
+            outputs = model(input1, input2)
+            y_test_labels.extend(list(outputs.detach().cpu()))
+        if not args.test:
+            test_csv = pd.read_csv('./data/test.csv', index_col=None)
+            test_csv['stars'] = y_test_labels
+            test_csv.to_csv('./data/test_pred.csv', index=False)
+    print('\n\n\n------------------------------------------\n'
+          'MIN RMSE valid {}\ttrain {} valid {}\n'
+          '-----------------------------------------------\n\n'.format(min_RMSE, train_rmse, valid_rmse))
